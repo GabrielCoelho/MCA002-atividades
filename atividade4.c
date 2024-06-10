800d2f4739ab8eee1d3db31c5f8be68af97b4e57
GabrielCoelho <gbcoe@outlook.com.br>
2024-06-10 19:17:25 -0300

    Initial Commit

---
+++ atividade4.c
@@ -0,0 +1,19 @@
+/*Integrantes: 
+ * Gabriel Coelho Soares
+ * Gabriel Vinholi
+ * Gustavo Poletini
+ * Marcos Moreira
+ * Thaíto Gabriel Batalini
+ * */
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  int op = 0;
+  while (op != 4) {
+    printf("Escolha uma opção para calcular a integral = \\int(a,b)f(x)dx:\n\t1.f(x) = x^k\n\t2.f(x) = 1/x\n\tf(x) = cos(x)\n\t4. SAIR\n\n Digite: ");
+  }
+
+
+
+}
